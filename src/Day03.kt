dce21c917688d5ec825cccbcc540c4bc35f08621
Thomas Bjornstad <thomas-bjornstad@nhn.no>
2022-12-03 13:27:26 +0100

    Oppgave 2 arbeid

--- src/Day03.kt
+++ src/Day03.kt
@@ -3,7 +3,7 @@ fun main() {
 
 
     fun getValueOfChar(char: Char): Int {
-        println(char)
+//        println(char)
         if (char.toInt() in 97..122)
             return char.toInt() - 96 //Fjerner 96 fra verdien, da ASCII for Liten bokstav, starter på 97 (a)
         else
@@ -21,10 +21,8 @@ fun main() {
             }
             if (commonChars.isLetter()) {
                 return getValueOfChar(commonChars)
-
             }
         }
-
         println("Fant INGEN?!?!?!? i: " + firstPart + " | " + secondPart)
 
        return 0
@@ -40,8 +38,8 @@ fun main() {
                 val mid = it.length / 2
                 val part1 = it.substring(0, mid)
                 val part2 = it.substring(mid)
-                println("Del 1: " + part1)
-                println("Del 2: " + part2)
+//                println("Del 1: " + part1)
+//                println("Del 2: " + part2)
                 val valueOfChar = checkForCommonCharAndGetValue(part1, part2)
                 score += valueOfChar
             }
@@ -49,6 +47,39 @@ fun main() {
         return score.toString()
     }
 
+    fun splitStringPart2(input: String): String {
+        val inputSplit = input.split("\r\n")
+
+        val a = inputSplit.forEachIndexed { index, s ->
+            println(index)
+            print(s)
+            val list: ArrayList<String> = ArrayList()
+            var counter = 0
+
+            for (counter in 0..2 ) {
+                
+            }
+
+            if (index % 3 == 0 || index == 0) {
+                list.add(s)
+                println(list)
+            }
+            else {
+
+            }
+
+
+
+        }
+
+        /* val list = listOf(inputSplit)
+        val listAsThree = list.filter {
+            println("Nummer: " + it)
+            when (it. % 3 == 0)
+        }
+*/
+        return "hei"
+    }
 
     fun part1(input: String): String {
         return splitString(input)
@@ -56,12 +87,11 @@ fun main() {
 
 
     fun part2(input: String): String {
-
-        return "hei"
+        return splitStringPart2(input)
     }
 
     val input = readInput("input_Day03")
 
-    println("Mennesket scorer i Runde 1: " + part1(input))
-//    println("Mennesket scorer i Runde 2: " + part2(input))
+//    println("Sum av bokstaver (Del 1): " + part1(input))
+    println("Sum av authentication-badge (Del 2): " + part2(input))
 }

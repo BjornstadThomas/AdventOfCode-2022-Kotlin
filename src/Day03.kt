98c13c2db8dfdbd232a86fa4ef8ac5524acfdb0c
Thomas Bjornstad <thomas-bjornstad@nhn.no>
2022-12-03 10:01:33 +0100

    Oppgave 1

--- src/Day03.kt
+++ src/Day03.kt
@@ -3,38 +3,55 @@ fun main() {
 
 
     fun getValueOfChar(char: Char): Int {
-
-
-        return 1
+        println(char)
+        if (char.toInt() in 97..122)
+            return char.toInt() - 96 //Fjerner 96 fra verdien, da ASCII for Liten bokstav, starter på 97 (a)
+        else
+            return char.toInt() - 38 //Fjerner kun 38, da ASCII for Stor bokstav, starter på 65 (A)
     }
 
-    fun checkForCommonChar(firstPart: String, secondPart: String): Char {
+    fun checkForCommonCharAndGetValue(firstPart: String, secondPart: String): Int {
+        var commonChars = ' '
 
+        for (i in 0 until firstPart.length) {
+            for (j in 0 until secondPart.length) {
+                if (firstPart[i] == secondPart[j]) {
+                    commonChars = firstPart[i]
+                }
+            }
+            if (commonChars.isLetter()) {
+                return getValueOfChar(commonChars)
 
-        return 'A'
+            }
+        }
+
+        println("Fant INGEN?!?!?!? i: " + firstPart + " | " + secondPart)
+
+       return 0
     }
 
 
     fun splitString(input: String): String {
         val inputSplit = input.split("\r\n")
-        val score = inputSplit
+        var score = 0
+
+        inputSplit
             .map {
                 val mid = it.length / 2
                 val part1 = it.substring(0, mid)
                 val part2 = it.substring(mid)
                 println("Del 1: " + part1)
                 println("Del 2: " + part2)
-                val common = checkForCommonChar(part1, part2)
-                val value = getValueOfChar(common)
-            }.toString()
+                val valueOfChar = checkForCommonCharAndGetValue(part1, part2)
+                score += valueOfChar
+            }
 
-        return score
+        return score.toString()
     }
 
 
     fun part1(input: String): String {
-        splitString(input)
-        return "hei"
+        return splitString(input)
     }
 
 
